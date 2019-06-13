14aeab2524904e83474adf82ae6072cae50ba364
Shenoy, Prasanth <un3795@corp.Alfacompanies.com>
2019-06-13 15:15:54 -0500

    updated the devdependencies npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,44 @@
-const path = require("path");
+const path = require('path');
+const CleanTerminalPlugin = require('clean-terminal-webpack-plugin');
 
 module.exports = {
   entry: {
     vendor: [
-      "angular",
-      "angular-animate",
-      "angular-route",
-      "angular-sanitize",
-      "angular-messages",
-      "angular-mocks",
-      "angular-ui-bootstrap",
-      "angular-toastr",
-      "lodash"
+      'angular',
+      'angular-animate',
+      'angular-route',
+      'angular-sanitize',
+      'angular-messages',
+      'angular-mocks',
+      'angular-ui-bootstrap',
+      'angular-toastr',
+      'lodash'
     ],
     app: [
-      "./js/app/app.mainmodule.js",
-      "./js/app/shared/backendmock/backendmock.js",
-      "./js/app/shared/toastr-service/toastr-service.js",
-      "./js/app/shared/app-data-service/app-data-service.js",
-      "./js/app/shared/directives/selectoption.js",
-      "./js/app/home/modals/rewardsmodal.component.js",
-      "./js/app/home/modals/redeemmodal.component.js",
-      "./js/app/home/home.component.js",
-      "./js/app/home/rewardssummary.component.js",
-      "./js/app/home/comments.component.js",
-      "./js/app/home/rewardinfo.component.js"
+      './js/app/app.mainmodule.js',
+      './js/app/shared/backendmock/backendmock.js',
+      './js/app/shared/toastr-service/toastr-service.js',
+      './js/app/shared/app-data-service/app-data-service.js',
+      './js/app/shared/directives/selectoption.js',
+      './js/app/home/modals/rewardsmodal.component.js',
+      './js/app/home/modals/redeemmodal.component.js',
+      './js/app/home/home.component.js',
+      './js/app/home/rewardssummary.component.js',
+      './js/app/home/comments.component.js',
+      './js/app/home/rewardinfo.component.js'
     ]
   },
   output: {
-    filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "public")
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   //this is needed for a good debugging exprience in devtools
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/
         // query: {
         //     presets: ["@babel/preset-env","@babel/preset-react"]
@@ -45,5 +46,10 @@ module.exports = {
         // }
       }
     ]
-  }
+  },
+  plugins: [
+    // Other plugins.
+    // WebpackClearConsole plugin removes all statements beginning with console.
+    new CleanTerminalPlugin()
+  ]
 };
